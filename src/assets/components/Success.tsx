9a0c21285dd91fc74442e2acdc2d254198eb0be0
JotaPeans <jpfontesferreira1@gmail.com>
2023-02-24 01:52:47 -0300

    update

--- src/assets/components/Success.tsx
+++ src/assets/components/Success.tsx
@@ -5,7 +5,7 @@ const Success = () => {
     const { success } = useContext(AppContext);
     return (
         <div className="fixed pointer-events-none top-0 left-0 w-screen h-screen flex justify-center items-center z-50">
-            <div className={`absolute top-0 bg-emerald-400/95 shadow-xl font-semibold font-sans p-3 rounded-lg transition-all ${success ? " top-4" : " -top-52"}`}>
+            <div className={`absolute bg-emerald-400/95 shadow-xl font-semibold font-sans p-3 rounded-lg transition-all ${success ? " top-4" : " -top-52"}`}>
                 Mensagem Criada!
             </div>
         </div>
