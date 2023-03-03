332cc02627e8edbbe24f1acaef8fab9f0c28a545
JotaPeans <jpfontesferreira1@gmail.com>
2023-03-03 10:17:09 -0300

    update

--- src/assets/components/Input.tsx
+++ src/assets/components/Input.tsx
@@ -1,13 +1,14 @@
 import React, { useState, useRef } from "react";
 
 interface IInput {
-    inputType: "email" | "password" | "cpf" | "text" | "submit",
-    refInput?: React.RefObject<HTMLInputElement>,
+    inputType: "email" | "password" | "cpf" | "text",
+    refInput: React.RefObject<HTMLInputElement>,
 }
 
 const Input = ({inputType = "text", refInput }: IInput) => {
     const ref = useRef<HTMLInputElement>(null);
     const [labelState, setlabelState] = useState(false);
+    const [key, setKey] = useState("");
 
     function handleFocus() {
         ref.current?.value;
@@ -18,37 +19,51 @@ const Input = ({inputType = "text", refInput }: IInput) => {
         !ref.current?.value ? setlabelState(false) : setlabelState(true);
     }
 
-    function maskCpf() {
+    function maskCpf(key: string) {
+        
         const input = ref.current;
 
-        if(input?.value.length === 3 || input?.value.length === 7) {
-            input.value = input.value + ".";
+        //console.log(key);
+        
+
+        const numberKeys = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+        if(key in numberKeys) {
+            if(( input?.value.length === 3 || input?.value.length === 7 ) && key !== "Backspace") {
+                input.value = input.value + ".";
+            }
+            else if(input?.value.length === 11 && key !== "Backspace") {
+                input.value = input.value + "-";
+            }
         }
-        else if(input?.value.length === 11) {
-            input.value = input.value + "-";
-        }  
+        else {
+            //TODO apagar ultimo digito
+        }
+
     }
 
-    function handleChange() {
+    function handleChange(e: React.KeyboardEvent<HTMLInputElement>) {
+        const key = e.key;
+        
         if(inputType === "cpf") {
-            maskCpf();
+            maskCpf(key);
 
-            if(refInput?.current && ref.current) {
+            if(refInput.current && ref.current) {
                 refInput.current.value = ref.current.value.replace(/\D/g, "");
             }
             return
         }
 
-        if(refInput?.current && ref.current) {
+        if(refInput.current && ref.current) {
             refInput.current.value = ref.current.value;
         }
     }
 
     return (
         <div className="flex flex-col justify-center relative">
-            <label className={`absolute transition-all ${labelState ? " -translate-y-11" : null} left-4 text-xl ${inputType === "cpf" ? "uppercase" : "capitalize"}`} htmlFor={inputType}>{inputType !== "submit" ? inputType : null}</label>
+            <label className={`absolute transition-all ${labelState ? " -translate-y-11" : null} left-4 text-xl ${inputType === "cpf" ? "uppercase" : "capitalize"}`} htmlFor={inputType}>{inputType}</label>
             <input className="hidden" ref={refInput} type="text" />
-            <input maxLength={inputType === "cpf" ? 14 : undefined} value={inputType === "submit" ? inputType : undefined} type={inputType} onChange={handleChange} onBlur={handleBlur} onFocus={handleFocus} ref={ref} className={` h-12 bg-slate-900/40 rounded-xl px-4 text-xl ${inputType === "submit" ? "cursor-pointer hover:bg-slate-900/70 transition-all" : null}`} id={inputType} />
+            <input maxLength={inputType === "cpf" ? 14 : undefined} type={inputType} onKeyDown={e => handleChange(e)} onBlur={handleBlur} onFocus={handleFocus} ref={ref} className=" h-12 bg-slate-900/40 rounded-xl px-4 text-xl" id={inputType} />
         </div>
     );
 }
