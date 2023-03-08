53c2b8f246645a6ebb6ca3d1f3e08ceb1f19f075
João Pedro <63313892+JotaPeans@users.noreply.github.com>
2023-03-08 01:57:45 -0300

    Update Card.tsx

--- src/assets/components/Card.tsx
+++ src/assets/components/Card.tsx
@@ -8,7 +8,7 @@ interface ICards {
 
 const Card = ({ message, date, isNew }: ICards) => {
     return (
-        <div className="relative z-10 bg-slate-300/40 w-full min-h-[10rem] h-auto rounded-lg shadow-lg py-2 px-3">
+        <div className="relative z-10 bg-slate-300/40 w-full min-h-[10rem] h-auto rounded-lg shadow-lg pt-2 pb-8 px-3">
             <p className="text-justify">{message}</p>
             <span className="absolute bottom-2 right-2 text-xs text-slate-300">{date}</span>
             {isNew ? <span className="absolute bottom-2 left-2 text-xs text-red-400 font-bold">Nova!</span> : null}
